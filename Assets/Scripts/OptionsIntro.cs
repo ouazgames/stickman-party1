a90fe897ae53449b6b68e8af231f187d8b5109b2
OuazGames <tredyapp1@gmail.com>
2023-04-18 16:50:06 +0200

    ccccccc

--- Assets/Scripts/OptionsIntro.cs
+++ Assets/Scripts/OptionsIntro.cs
@@ -2,76 +2,76 @@ using UnityEngine;
 
 public class OptionsIntro : MonoBehaviour
 {
-	public int AudioCheck;
+    public int AudioCheck;
 
-	public int VibrationCheck;
+    public int VibrationCheck;
 
-	public static OptionsIntro Instance
-	{
-		get;
-		set;
-	}
+    public static OptionsIntro Instance
+    {
+        get;
+        set;
+    }
 
-	private void Awake()
-	{
-		Instance = this;
-		Object.DontDestroyOnLoad(base.gameObject);
-		AudioCheck = PlayerPrefs.GetInt("AudioCheck");
-		VibrationCheck = PlayerPrefs.GetInt("VibrationCheck");
-		if (AudioCheck == 0)
-		{
-			AudioListener.volume = 1f;
-		}
-		else if (AudioCheck == 1)
-		{
-			AudioListener.volume = 0f;
-		}
-	}
+    private void Awake()
+    {
+        Instance = this;
+        Object.DontDestroyOnLoad(base.gameObject);
+        AudioCheck = PlayerPrefs.GetInt("AudioCheck");
+        VibrationCheck = PlayerPrefs.GetInt("VibrationCheck");
+        if (AudioCheck == 0)
+        {
+            AudioListener.volume = 1f;
+        }
+        else if (AudioCheck == 1)
+        {
+            AudioListener.volume = 0f;
+        }
+    }
 
-	private void OnDisable()
-	{
-	}
+    private void OnDisable()
+    {
+    }
 
-	public void OffAllSound()
-	{
-		AudioCheck = 1;
-		AudioListener.volume = 0f;
-		PlayerPrefs.SetInt("AudioCheck", AudioCheck);
-	}
+    public void OffAllSound()
+    {
+        AudioCheck = 1;
+        AudioListener.volume = 0f;
+        PlayerPrefs.SetInt("AudioCheck", AudioCheck);
+    }
 
-	public void OnAllSound()
-	{
-		AudioCheck = 0;
-		AudioListener.volume = 1f;
-		PlayerPrefs.SetInt("AudioCheck", AudioCheck);
-	}
+    public void OnAllSound()
+    {
+        AudioCheck = 0;
+        AudioListener.volume = 1f;
+        PlayerPrefs.SetInt("AudioCheck", AudioCheck);
+    }
 
-	public void OffVibration()
-	{
-		VibrationCheck = 1;
-		PlayerPrefs.SetInt("VibrationCheck", VibrationCheck);
-	}
+    public void OffVibration()
+    {
+        VibrationCheck = 1;
+        PlayerPrefs.SetInt("VibrationCheck", VibrationCheck);
+    }
 
-	public void OnVibration()
-	{
-		VibrationCheck = 0;
-		TriggerSelection();
-		PlayerPrefs.SetInt("VibrationCheck", VibrationCheck);
-	}
+    public void OnVibration()
+    {
+        VibrationCheck = 0;
+        TriggerSelection();
+        PlayerPrefs.SetInt("VibrationCheck", VibrationCheck);
+    }
 
-	public virtual void TriggerVibrate()
-	{
-		if (VibrationCheck == 0)
-		{
-			MMVibrationManager.Vibrate();
-		}
-	}
+    public virtual void TriggerVibrate()
+    {
+        if (VibrationCheck == 0)
+        {
+            // MMVibrationManager.Vibrate();
+        }
+    }
 
-	public virtual void TriggerSelection()
-	{
-		if (VibrationCheck == 0)
-		{
-			MMVibrationManager.Haptic(HapticTypes.Selection);
-		}
-	}
+    public virtual void TriggerSelection()
+    {
+        if (VibrationCheck == 0)
+        {
+            // MMVibrationManager.Haptic(HapticTypes.Selection);
+        }
+    }
 }
--- Assets/Scripts/SoccerGameController.cs
+++ Assets/Scripts/SoccerGameController.cs
@@ -4,340 +4,347 @@ using UnityEngine.UI;
 
 public class SoccerGameController : MonoBehaviour
 {
-	public ShowAds SA;
+    public ShowAds SA;
 
-	public GameObject[] GoalAnimation;
+    public GameObject[] GoalAnimation;
 
-	public int RedGoals;
+    public int RedGoals;
 
-	public int BlueGoals;
+    public int BlueGoals;
 
-	private float time = 120f;
+    private float time = 120f;
 
-	public Text[] TimerText;
+    public Text[] TimerText;
 
-	public Text[] RedGoalsText;
+    public Text[] RedGoalsText;
 
-	public Text[] BlueGoalsText;
+    public Text[] BlueGoalsText;
 
-	public Transform[] Gametransform;
+    public Transform[] Gametransform;
 
-	public GameObject BallObject;
+    public GameObject BallObject;
 
-	public GameObject BGMusic;
+    public GameObject BGMusic;
 
-	public GameObject GameOverPanel;
+    public GameObject GameOverPanel;
 
-	public GameObject[] WinObject;
+    public GameObject[] WinObject;
 
-	public GameObject GameOverButtons;
+    public GameObject GameOverButtons;
 
-	public GameObject GameOverTie;
+    public GameObject GameOverTie;
 
-	public GameObject TimerAnimation;
+    public GameObject TimerAnimation;
 
-	private bool IsStart;
+    private bool IsStart;
 
-	public AudioClip ButtonSound;
+    public AudioClip ButtonSound;
 
-	public AudioClip Sound3;
+    public AudioClip Sound3;
 
-	public AudioClip Sound2;
+    public AudioClip Sound2;
 
-	public AudioClip Sound1;
+    public AudioClip Sound1;
 
-	public AudioClip SoundGo;
+    public AudioClip SoundGo;
 
-	public AudioClip GoalSound;
+    public AudioClip GoalSound;
 
-	public AudioClip ReferiSound;
+    public AudioClip ReferiSound;
 
-	public AudioClip TimerSound;
+    public AudioClip TimerSound;
 
-	private float CountDownSoundTimer = 2f;
+    private float CountDownSoundTimer = 2f;
 
-	private AudioSource Source;
+    private AudioSource Source;
 
-	public GameObject[] Player;
+    public GameObject[] Player;
 
-	public GameObject[] JoystickObject;
+    public GameObject[] JoystickObject;
 
-	private int PlayerNumers;
+    private int PlayerNumers;
 
-	private GameObject[] ActivePlayers;
+    private GameObject[] ActivePlayers;
 
-	private string PlayerName;
+    private string PlayerName;
 
-	private bool IsGameOver;
+    private bool IsGameOver;
 
-	public int ActiveP;
+    public int ActiveP;
 
-	private int NeedBot;
+    private int NeedBot;
 
-	public GameObject AiPlayer;
+    public GameObject AiPlayer;
 
-	private void Awake()
-	{
-		PlayerNumers = PlayerPrefs.GetInt("PlayerNumers");
-		NeedBot = PlayerPrefs.GetInt("NeedBot");
-	}
+    private void Awake()
+    {
+        PlayerNumers = PlayerPrefs.GetInt("PlayerNumers");
+        NeedBot = PlayerPrefs.GetInt("NeedBot");
+    }
 
-	private void Start()
-	{
-		//SA = Camera.main.GetComponent<ShowAds>();
-		RedGoals = 0;
-		BlueGoals = 0;
-		if (PlayerNumers == 1)
-		{
-			Player[0].SetActive(value: true);
-			AiPlayer.GetComponent<SoccerPlayer>().IsAIPlayer = true;
-			AiPlayer.SetActive(value: true);
-			JoystickObject[0].SetActive(value: true);
-		}
-		else if (PlayerNumers == 2)
-		{
-			Player[0].SetActive(value: true);
-			Player[1].SetActive(value: true);
-			JoystickObject[0].SetActive(value: true);
-			JoystickObject[1].SetActive(value: true);
-		}
-		else if (PlayerNumers == 3)
-		{
-			Player[0].SetActive(value: true);
-			Player[1].SetActive(value: true);
-			Player[2].SetActive(value: true);
-			JoystickObject[0].SetActive(value: true);
-			JoystickObject[1].SetActive(value: true);
-			JoystickObject[2].SetActive(value: true);
-		}
-		else if (PlayerNumers == 4)
-		{
-			GameObject[] player = Player;
-			for (int i = 0; i < player.Length; i++)
-			{
-				player[i].SetActive(value: true);
-			}
-			player = JoystickObject;
-			for (int i = 0; i < player.Length; i++)
-			{
-				player[i].SetActive(value: true);
-			}
-		}
-		Source = GetComponent<AudioSource>();
-		Source.volume = 0.8f;
-		Invoke("StartAnimation", 0.5f);
-		Invoke("StartGame", 1.2f);
-		ActivePlayers = GameObject.FindGameObjectsWithTag("Player");
-		ActiveP = ActivePlayers.Length;
-		SetTransform();
-	}
+    private void Start()
+    {
+        //SA = Camera.main.GetComponent<ShowAds>();
+        RedGoals = 0;
+        BlueGoals = 0;
+        if (PlayerNumers == 1)
+        {
+            Player[0].SetActive(value: true);
+            AiPlayer.GetComponent<SoccerPlayer>().IsAIPlayer = true;
+            AiPlayer.SetActive(value: true);
+            JoystickObject[0].SetActive(value: true);
+        }
+        else if (PlayerNumers == 2)
+        {
+            Player[0].SetActive(value: true);
+            Player[1].SetActive(value: true);
+            JoystickObject[0].SetActive(value: true);
+            JoystickObject[1].SetActive(value: true);
+        }
+        else if (PlayerNumers == 3)
+        {
+            Player[0].SetActive(value: true);
+            Player[1].SetActive(value: true);
+            Player[2].SetActive(value: true);
+            JoystickObject[0].SetActive(value: true);
+            JoystickObject[1].SetActive(value: true);
+            JoystickObject[2].SetActive(value: true);
+        }
+        else if (PlayerNumers == 4)
+        {
+            GameObject[] player = Player;
+            for (int i = 0; i < player.Length; i++)
+            {
+                player[i].SetActive(value: true);
+            }
+            player = JoystickObject;
+            for (int i = 0; i < player.Length; i++)
+            {
+                player[i].SetActive(value: true);
+            }
+        }
+        Source = GetComponent<AudioSource>();
+        Source.volume = 0.8f;
+        Invoke("StartAnimation", 0.5f);
+        Invoke("StartGame", 1.2f);
+        ActivePlayers = GameObject.FindGameObjectsWithTag("Player");
+        ActiveP = ActivePlayers.Length;
+        SetTransform();
+    }
 
-	private void StartAnimation()
-	{
-		TimerAnimation.SetActive(value: true);
-		Source.PlayOneShot(SoundGo);
-	}
+    private void StartAnimation()
+    {
+        TimerAnimation.SetActive(value: true);
+        Source.PlayOneShot(SoundGo);
+    }
 
-	private void StartGame()
-	{
-		IsStart = true;
-		GameObject[] activePlayers = ActivePlayers;
-		for (int i = 0; i < activePlayers.Length; i++)
-		{
-			activePlayers[i].GetComponent<SoccerPlayer>().IsSMove = true;
-		}
-		Invoke("Referi", 0.5f);
-		StartCoundownTimer();
-	}
+    private void StartGame()
+    {
+        IsStart = true;
+        GameObject[] activePlayers = ActivePlayers;
+        for (int i = 0; i < activePlayers.Length; i++)
+        {
+            activePlayers[i].GetComponent<SoccerPlayer>().IsSMove = true;
+        }
+        Invoke("Referi", 0.5f);
+        StartCoundownTimer();
+    }
 
-	private void StartCoundownTimer()
-	{
-		time = 120f;
-		Text[] timerText = TimerText;
-		for (int i = 0; i < timerText.Length; i++)
-		{
-			timerText[i].text = "02:00";
-		}
-		InvokeRepeating("UpdateTimer", 0f, 0.01667f);
-	}
+    private void StartCoundownTimer()
+    {
+        time = 120f;
+        Text[] timerText = TimerText;
+        for (int i = 0; i < timerText.Length; i++)
+        {
+            timerText[i].text = "02:00";
+        }
+        InvokeRepeating("UpdateTimer", 0f, 0.01667f);
+    }
 
-	private void UpdateTimer()
-	{
-		if (!IsGameOver)
-		{
-			time -= Time.deltaTime;
-		}
-		string str = Mathf.Floor(time / 60f).ToString("00");
-		string str2 = (time % 60f).ToString("00");
-		Text[] timerText = TimerText;
-		for (int i = 0; i < timerText.Length; i++)
-		{
-			timerText[i].text = str + ":" + str2;
-		}
-	}
+    private void UpdateTimer()
+    {
+        if (!IsGameOver)
+        {
+            time -= Time.deltaTime;
+        }
+        string str = Mathf.Floor(time / 60f).ToString("00");
+        string str2 = (time % 60f).ToString("00");
+        Text[] timerText = TimerText;
+        for (int i = 0; i < timerText.Length; i++)
+        {
+            timerText[i].text = str + ":" + str2;
+        }
+    }
 
-	private void Update()
-	{
-		AdsManager.Instance.HideBanner();
-		if (time <= 2f && time > 0f && !IsGameOver && CountDownSoundTimer >= 2f)
-		{
-			Source.PlayOneShot(TimerSound);
-			CountDownSoundTimer = 0f;
-		}
-		if (time <= 0f && !IsGameOver)
-		{
-			time = 0f;
-			GameOver();
-			
-			IsGameOver = true;
-		}
-	}
+    private void Update()
+    {
+        AdsManager.Instance.HideBanner();
+        if (time <= 2f && time > 0f && !IsGameOver && CountDownSoundTimer >= 2f)
+        {
+            Source.PlayOneShot(TimerSound);
+            CountDownSoundTimer = 0f;
+        }
+        if (time <= 0f && !IsGameOver)
+        {
+            time = 0f;
+            GameOver();
 
-	private void Referi()
-	{
-		Source.volume = 0.8f;
-		Source.PlayOneShot(ReferiSound);
-	}
+            IsGameOver = true;
+        }
+    }
 
-	public void RedGoal()
-	{
-		Source.volume = 0.45f;
-		Source.PlayOneShot(GoalSound);
-		GoalAnimation[0].SetActive(value: true);
-		RedGoals++;
-		Text[] redGoalsText = RedGoalsText;
-		for (int i = 0; i < redGoalsText.Length; i++)
-		{
-			redGoalsText[i].text = string.Concat(RedGoals);
-		}
-		BallObject.SetActive(value: false);
-		if (RedGoals < 3)
-		{
-			Invoke("SetTransform", 2.5f);
-			Invoke("Referi", 2.7f);
-		}
-		else
-		{
-			GameOver();
-		}
-	}
+    private void Referi()
+    {
+        Source.volume = 0.8f;
+        Source.PlayOneShot(ReferiSound);
+    }
 
-	public void BlueGoal()
-	{
-		Source.volume = 0.45f;
-		Source.PlayOneShot(GoalSound);
-		GoalAnimation[1].SetActive(value: true);
-		BlueGoals++;
-		Text[] blueGoalsText = BlueGoalsText;
-		for (int i = 0; i < blueGoalsText.Length; i++)
-		{
-			blueGoalsText[i].text = string.Concat(BlueGoals);
-		}
-		BallObject.SetActive(value: false);
-		if (BlueGoals < 3)
-		{
-			Invoke("SetTransform", 2.5f);
-			Invoke("Referi", 2.7f);
-		}
-		else
-		{
-			GameOver();
-		}
-	}
+    public void RedGoal()
+    {
+        Source.volume = 0.45f;
+        Source.PlayOneShot(GoalSound);
+        GoalAnimation[0].SetActive(value: true);
+        RedGoals++;
+        Text[] redGoalsText = RedGoalsText;
+        for (int i = 0; i < redGoalsText.Length; i++)
+        {
+            redGoalsText[i].text = string.Concat(RedGoals);
+        }
+        BallObject.SetActive(value: false);
+        if (RedGoals < 3)
+        {
+            Invoke("SetTransform", 2.5f);
+            Invoke("Referi", 2.7f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
 
-	public void GameOver()
-	{
-		IsGameOver = true;
-		GameAnalytic.AddProgression(new LevelProgression()
-		{
+    public void BlueGoal()
+    {
+        Source.volume = 0.45f;
+        Source.PlayOneShot(GoalSound);
+        GoalAnimation[1].SetActive(value: true);
+        BlueGoals++;
+        Text[] blueGoalsText = BlueGoalsText;
+        for (int i = 0; i < blueGoalsText.Length; i++)
+        {
+            blueGoalsText[i].text = string.Concat(BlueGoals);
+        }
+        BallObject.SetActive(value: false);
+        if (BlueGoals < 3)
+        {
+            Invoke("SetTransform", 2.5f);
+            Invoke("Referi", 2.7f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
 
-			state = ProgressionStatus.Completed,
-			v_LevelName = "Level Soccer Game"
+    public void GameOver()
+    {
+        IsGameOver = true;
+        GameAnalytic.AddProgression(new LevelProgression()
+        {
 
-		});
-		//GameOverPanel.SetActive(value: true);
-		BGMusic.SetActive(value: false);
-		AdsManager.Instance.ShowIntersitial(() => { Debug.Log("intersitial worked"); });
-		if (PlayerPrefs.GetInt("IsCups") == 0)
-		{
-			Invoke("GetButtons", 2f);
-		}
-		else if (PlayerPrefs.GetInt("IsCups") == 1)
-		{
-			Invoke("GetCount", 3f);
-		}
-		if (RedGoals > BlueGoals)
-		{
-			WinObject[0].SetActive(value: true);
-			if (ActivePlayers.Length != 4)
-			{
-				return;
-			}
-			WinObject[3].SetActive(value: true);
-			if (PlayerPrefs.GetInt("IsCups") == 1)
-			{
-				PlayerPrefs.SetInt("RedNewCount", 3);
-				PlayerPrefs.SetInt("BlueNewCount", 0);
-			}
-		}
-		else if (RedGoals < BlueGoals)
-		{
-			WinObject[1].SetActive(value: true);
-			if (ActivePlayers.Length < 3)
-			{
-				return;
-			}
-			WinObject[2].SetActive(value: true);
-			if (PlayerPrefs.GetInt("IsCups") == 1)
-			{
-				PlayerPrefs.SetInt("BlueNewCount", 3);
-				PlayerPrefs.SetInt("RedNewCount", 0);
-			}
-		}
-		else if (RedGoals == BlueGoals)
-		{
-			GameOverTie.SetActive(value: true);
-		}
-		GameObject[] activePlayers = ActivePlayers;
-		for (int i = 0; i < activePlayers.Length; i++)
-		{
-			activePlayers[i].GetComponent<SoccerPlayer>().IsSMove = false;
-		}
-		//SA.ShowInter();
-	}
+            state = ProgressionStatus.Completed,
+            v_LevelName = "Level Soccer Game"
 
-	private void GetCount()
-	{
-		SceneManager.LoadScene("TournamentSceneVS");
-	}
+        });
+        //GameOverPanel.SetActive(value: true);
+        BGMusic.SetActive(value: false);
+        AdsManager.Instance.ShowIntersitial(() => { Debug.Log("intersitial worked"); });
+        if (PlayerPrefs.GetInt("IsCups") == 0)
+        {
+            Invoke("GetButtons", 2f);
+        }
+        else if (PlayerPrefs.GetInt("IsCups") == 1)
+        {
+            Invoke("GetCount", 3f);
+        }
+        if (RedGoals > BlueGoals)
+        {
+            WinObject[0].SetActive(value: true);
+            if (ActivePlayers.Length != 4)
+            {
+                return;
+            }
+            WinObject[3].SetActive(value: true);
+            if (PlayerPrefs.GetInt("IsCups") == 1)
+            {
+                PlayerPrefs.SetInt("RedNewCount", 3);
+                PlayerPrefs.SetInt("BlueNewCount", 0);
+            }
+        }
+        else if (RedGoals < BlueGoals)
+        {
+            WinObject[1].SetActive(value: true);
+            if (ActivePlayers.Length < 3)
+            {
+                return;
+            }
+            WinObject[2].SetActive(value: true);
+            if (PlayerPrefs.GetInt("IsCups") == 1)
+            {
+                PlayerPrefs.SetInt("BlueNewCount", 3);
+                PlayerPrefs.SetInt("RedNewCount", 0);
+            }
+        }
+        else if (RedGoals == BlueGoals)
+        {
+            GameOverTie.SetActive(value: true);
+        }
+        GameObject[] activePlayers = ActivePlayers;
+        for (int i = 0; i < activePlayers.Length; i++)
+        {
+            activePlayers[i].GetComponent<SoccerPlayer>().IsSMove = false;
+        }
+        //SA.ShowInter();
+    }
 
-	private void GetButtons()
-	{
-		GameOverButtons.SetActive(value: true);
-	}
+    private void GetCount()
+    {
+        SceneManager.LoadScene("TournamentSceneVS");
+    }
 
-	public void MainMenuButton()
-	{
-		Source.PlayOneShot(ButtonSound);
-		SceneManager.LoadScene(1);
-	}
+    private void GetButtons()
+    {
+        GameOverButtons.SetActive(value: true);
+    }
 
-	public void RestartButton()
-	{
-		Source.PlayOneShot(ButtonSound);
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-	}
+    public void MainMenuButton()
+    {
+        AdsManager.Instance.ShowIntersitial(() =>
+        {
+            Source.PlayOneShot(ButtonSound);
+            SceneManager.LoadScene(1);
+        });
+    }
 
-	private void SetTransform()
-	{
-		Player[0].transform.position = Gametransform[0].transform.position;
-		Player[1].transform.position = Gametransform[1].transform.position;
-		AiPlayer.transform.position = Gametransform[1].transform.position;
-		Player[2].transform.position = Gametransform[2].transform.position;
-		Player[3].transform.position = Gametransform[3].transform.position;
-		BallObject.transform.position = Gametransform[4].transform.position;
-		GoalAnimation[0].SetActive(value: false);
-		GoalAnimation[1].SetActive(value: false);
-		BallObject.SetActive(value: true);
-	}
+    public void RestartButton()
+    {
+        AdsManager.Instance.ShowInterstitialTimer(() =>
+        {
+
+            Source.PlayOneShot(ButtonSound);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        });
+    }
+
+    private void SetTransform()
+    {
+        Player[0].transform.position = Gametransform[0].transform.position;
+        Player[1].transform.position = Gametransform[1].transform.position;
+        AiPlayer.transform.position = Gametransform[1].transform.position;
+        Player[2].transform.position = Gametransform[2].transform.position;
+        Player[3].transform.position = Gametransform[3].transform.position;
+        BallObject.transform.position = Gametransform[4].transform.position;
+        GoalAnimation[0].SetActive(value: false);
+        GoalAnimation[1].SetActive(value: false);
+        BallObject.SetActive(value: true);
+    }
 }
